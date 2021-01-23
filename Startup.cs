0262b01098602c168a7a38d60e3d6faf2ebd40e9
Martin11175 <martinhiggs85@googlemail.com>
2021-01-23 13:16:22 +0000

    feat(site): Add static files to serve UI

--- Startup.cs
+++ Startup.cs
@@ -44,6 +44,9 @@ namespace roundtable
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "roundtable v1"));
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
